c2aaf1081b4453d9493e32ecca754793183299e5
freediver <freediver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-26 15:16:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/worker/tags/3.9.0@389411 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ link.class.php
@@ -0,0 +1,39 @@
+<?php
+/*************************************************************
+ * 
+ * link.class.php
+ * 
+ * Add Links
+ * 
+ * 
+ * Copyright (c) 2011 Prelovac Media
+ * www.prelovac.com
+ **************************************************************/
+
+class MMB_Link extends MMB_Core
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function add_link($args)
+    {
+    	extract($args);
+    	
+    	$params['link_url'] = esc_html($url);
+			$params['link_url'] = esc_url($params['link_url']);
+			$params['link_name'] = esc_html($name);
+			$params['link_id'] = '';
+			$params['link_description'] = $description;
+			
+			if(!function_exists(wp_insert_link))
+			include_once (ABSPATH . 'wp-admin/includes/bookmark.php');
+			
+			$is_success = wp_insert_link($params);
+			
+			return $is_success ? true : array('error' => 'Failed to add link'); 
+    }
+    
+}
+?>
