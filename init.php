151532f4d109f802af27a4da3d14f230bb24db58
freediver <freediver@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-22 16:08:32 +0000

    Don't allow IWP to break our plugin
    git-svn-id: https://plugins.svn.wordpress.org/worker/trunk@1073440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -219,7 +219,7 @@ if (!function_exists('mwp_init')):
         $GLOBALS['MMB_WORKER_REVISION'] = '2015-01-19 00:00:00';
         $GLOBALS['mmb_plugin_dir']      = WP_PLUGIN_DIR.'/'.basename(dirname(__FILE__));
         $GLOBALS['_mmb_item_filter']    = array();
-        $GLOBALS['mmb_core']            = $core = $mmb_core_backup = new MMB_Core();
+        $GLOBALS['mmb_core']            = $core = $GLOBALS['mmb_core_backup'] = new MMB_Core();
 
         $siteUrl = function_exists('get_site_option') ? get_site_option('siteurl') : get_option('siteurl');
         define('MMB_XFRAME_COOKIE', 'wordpress_'.md5($siteUrl).'_xframe');
